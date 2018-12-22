92da527a3c4196fc72219d5173acc4dfdcb96337
lfernandez55 <lfernandez@weber.edu>
2018-12-21 23:40:56 -0700

    To testing the form view
    can include a test to make sure the user can reach the New topic view from this page
    
    ---------
    
     https://simpleisbetterthancomplex.com/series/2017/09/18/a-complete-beginners-guide-to-django-part-3.html

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
-from django.http import Http404
-from django.http import HttpResponse
-from .models import Board
+from django.shortcuts import render, redirect, get_object_or_404
+# from django.http import Http404
+# from django.http import HttpResponse
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
 
 # def home(request):
 #     # return HttpResponse('Hello, World!')
@@ -28,3 +27,28 @@ def board_topics(request, pk):
     #     raise Http404
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()  # TODO: get the currently logged in user
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic p
+
+    return render(request, 'new_topic.html', {'board': board})
